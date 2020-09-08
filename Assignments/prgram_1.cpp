162140e43491df14a51b87d12ee175163eeaa92c
Matthew <mgarcia495@yahoo.com>
2020-09-08 16:44:20 -0700

    movie name input

---
+++ Assignments/prgram_1.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	cout << "What is the name of the movie? \n";
+
+	string movie_name;
+
+	cin >> movie_name;
+
+	cout << "Movie Name: " << movie_name << "\n";
+}
