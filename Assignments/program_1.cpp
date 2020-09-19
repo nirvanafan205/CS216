a169c2b51d1e49195a6af41529e32be6f09b1a1f
Matthew <mgarcia495@yahoo.com>
2020-09-18 23:32:54 -0700

    comments made

--- Assignments/program_1.cpp
+++ Assignments/program_1.cpp
@@ -1,48 +1,17 @@
+/*
+ Write a program that computes and displays the charges for a patient’s hospital stay. First,
+the program should ask if the patient was admitted as an in-patient or an out-patient.
+Once the required data has been input and validated, the program should use two
+overloaded functions to calculate the total charges. One of the functions should accept
+arguments for the in-patient data, while the other function accepts arguments for out-
+patient data. Both functions should return the total charges.
+*/
+
 #include <iostream>
 
 using namespace std;
 
-void discounts(int price, int room_amount, int days, double tax);
-
 int main()
 {
-	cout << "What is the price of renting one room?\n";
-	int price;
-	cin >> price;
 
-	cout << "\nWhat are the number of rooms booked?\n";
-	int room_amount;
-	cin >> room_amount;
-
-	cout << "\nWhat is the number of days will you be staying?\n";
-	int days;
-	cin >> days;
-
-	cout <<"\nWhat is the tax %?\n";
-	double tax;
-	cin >> tax;
-	tax /= 100;
-
-
-	discounts(price, room_amount, days, tax);
-}
-
-void discounts(int price, int room_amount, int days, double tax)
-{
-	double taxe = (price * room_amount) * tax;
-
-	int total = (price * room_amount) + taxe;
-
-	int room_total = price * room_amount;
-
-	if(room_amount < 10 && days < 3)
-	{
-		cout << "\nCost of renting one room: " << price;
-		cout << "\nNo discount: 0%";
-		cout << "\nNumber of rooms booked: " << room_amount;
-		cout << "\ntotal cost of the rooms: " << room_total;
-		cout << "\nsales tax: " << taxe;
-		cout << "\ntotal billing: " << total;
-		cout <<"\n";
-	}	
 }
--- Assignments/program_2.cpp
+++ Assignments/program_2.cpp
@@ -1,53 +1,14 @@
+/*
+  Design an Inventory class that can hold information for an item in a retail store’s inventory.
+The class should have the following private member variables.
+This program should validate
+the user inputs to ensure that negative values are not accepted for item number, quantity, or cost.
+*/
+
 #include <iostream>
-#include <fstream>
 
 using namespace std;
 
-bool die(const string & msg);
-
 int main()
 {
-	ifstream readFile;
-	ofstream writeFile;
-
-	string lastName, firstName;
-	double salary, raise;
-
-	readFile.open("employeeInput.txt");
-	writeFile.open("employeeOutput.txt");
-
-	if(!readFile)
-	{
-		die("File does not exist\n");
-	}
-
-	else
-	{
-		while(readFile)
-		{
-			readFile >> lastName;
-			readFile >> firstName;
-			readFile >> salary;
-			readFile >> raise;
-
-			raise /= 100;
-
-			salary = (raise * salary) + salary;
-
-			if(!readFile)
-			{
-				break;
-			}
-
-			writeFile << firstName << " " << lastName << "	" << salary << "\n";
-		}
-
-		cout << "File has been written" << endl;
-	}
-}
-
-bool die(const string & msg)
-{
-	cout << "Fatal error: " << msg << "\n";
-	exit(EXIT_FAILURE);
 }
