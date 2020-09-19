c2e56e1e4d722f757edad8d9df3327909c1aa9c6
Matthew <mgarcia495@yahoo.com>
2020-09-19 00:22:22 -0700

    added private variables, setters and getters, started on input function

--- Assignments/program_2.cpp
+++ Assignments/program_2.cpp
@@ -1,14 +1,72 @@
 /*
-  Design an Inventory class that can hold information for an item in a retail store’s inventory.
-The class should have the following private member variables.
-This program should validate
-the user inputs to ensure that negative values are not accepted for item number, quantity, or cost.
+   Design an Inventory class that can hold information for an item in a retail store’s inventory.
+   The class should have the following private member variables.
+   This program should validate
+   the user inputs to ensure that negative values are not accepted for item number, quantity, or cost.
 */
 
 #include <iostream>
 
 using namespace std;
 
+class Inventory
+{
+	private:
+		int itemNumber;
+		int quantity;
+		double cost;
+
+	public:
+		void setZeros(int number, int amount, double price);
+
+		int getNum()
+		{
+			return itemNumber;
+		}
+
+		int getAmount()
+		{
+			return quantity;
+		}
+
+		double getPrice()
+		{
+			return cost;
+		}
+};
+
+void Inventory::setZeros(int number, int amount, double price)
+{
+	itemNumber = number;
+	quantity = amount;
+	cost = price;
+}
+
+
+void input(Inventory &item);
+
 int main()
 {
+	Inventory input;
+
+
+}
+
+
+void input(Inventory &item)
+{
+	int num;
+	int amount;
+	double price;
+
+	cout << "Enter data" << endl;
+
+	cout << "Item number" << endl;
+	cin >> num;
+
+	cout << "Quantity on hand" << endl;
+	cin >> amount;
+
+	cout << "Wholesale per-unit cost" << endl;
+	cin >> price;
 }
