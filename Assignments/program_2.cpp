d6f891b868a584c222787f60dc18f4bc78a90eb6
Matthew <mgarcia495@yahoo.com>
2020-09-29 13:06:27 -0700

    bubble sort

--- Assignments/program_2.cpp
+++ Assignments/program_2.cpp
@@ -3,7 +3,7 @@
 using namespace std;
 
 void bubbleSort(int sort[], int size);
-void selectionSort();
+void selectionSort(int sort[], int size);
 
 int main()
 {
@@ -26,13 +26,29 @@ int main()
 	}
 
 	bubbleSort(bubble_array, end);
+
+	for(int j = 0; j <= end; j++)
+	{
+		if(j == end)
+		{
+			cout << selection_array[j] << endl;
+		}
+
+		else 
+		{
+			cout << selection_array[j] << ", ";
+		}
+	}
+
+	selectionSort(selection_array, end);
+
 }
 
 void bubbleSort(int sort[], int size)
 {
-	for(int i = 0; i < size - 1; i++)
+	for(int i = 0; i < size; i++)
 	{
-		for(int j = 0; j < size - i - 1; j++)
+		for(int j = 0; j < size - i; j++)
 		{
 			if(sort[j] > sort[j + 1])
 			{
@@ -43,13 +59,21 @@ void bubbleSort(int sort[], int size)
 		}
 	}
 
-	for(int nums : sort[])
+	for(int i = 0; i <= size; i++)
 	{
-		cout << nums;
+		if(i == size)
+		{
+			cout << sort[i] << endl;
+		}
+
+		else 
+		{
+			cout << sort[i] << ", ";
+		}
 	}
 }
 
-void selectionSort()
+void selectionSort(int sort[], int size)
 {
 
 }
