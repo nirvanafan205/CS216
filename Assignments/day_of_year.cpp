88add42256923c5933d849fcb67a6a4dd3b165dc
Matthew <mgarcia495@yahoo.com>
2020-10-12 20:38:12 -0700

    added class and constructor

--- Assignments/day_of_year.cpp
+++ Assignments/day_of_year.cpp
@@ -9,113 +9,132 @@
   month-day format. The class should have an integer member variable to represent the day
   and should have static member variables of type string to assist in the translation from
   the integer format to the month–day format.
-  Test your class by inputting various integers representing days and printing out their
-  representation in the month–day format.
-*/
-
+  Test your class by inputting various integers representing days and printing out their representation in the month–day format.
+  */
 #include <iostream>
 
 using namespace std;
 
 bool die(const string & msg);
 
+class DayOfYear
+{
+	public: 
+
+	int day;
+	void initialize(int);
+
+	void print(int day)
+	{
+		string month;
+		if(day > 0 && day < 32)
+		{
+			month = "January";
+			cout << month << " " << day << endl;
+		}
+
+		else if(day > 31 && day < 61)
+		{
+			month = "February";
+			day -= 31;
+			cout << month << " " << day << endl;
+		}
+
+		else if(day > 60 && day < 92)
+		{
+			month = "March";
+			day -= 60;
+			cout << month << " " << day << endl;
+		}
+
+		else if(day > 91 && day < 122)
+		{
+			month = "April";
+			day -= 91;
+			cout << month << " " << day << endl;
+		}
+
+		else if(day > 121 && day < 153)
+		{
+			month = "May";
+			day -= 121;
+			cout << month << " " << day << endl;
+		}
+
+		else if(day > 152 && day < 183)
+		{
+			month = "June";
+			day -= 152;
+			cout << month << " " << day << endl;
+		}
+
+		else if(day > 182 && day < 214)
+		{
+			month = "July";
+			day -= 182;
+			cout << month << " " << day << endl;
+		}
+
+		else if(day > 213 && day < 245)
+		{
+			month = "August";
+			day -= 213;
+			cout << month << " " << day << endl;
+		}
+
+		else if(day > 244 && day < 275)
+		{
+			month = "September";
+			day -= 244;
+			cout << month << " " << day << endl;
+		}
+
+		else if(day > 274 && day < 306)
+		{
+			month = "October";
+			day -= 274;
+			cout << month << " " << day << endl;
+		}
+
+		else if(day > 305 && day < 336)
+		{
+			month = "November";
+			day -= 305;
+			cout << month << " " << day << endl;
+		}
+
+		else if(day > 335 && day < 367)
+		{
+			month = "December";
+			day -= 335;
+			cout << month << " " << day << endl;	
+		}
+
+		else
+		{
+			die("date does not exist");
+		}
+	}
+};
+
+void DayOfYear::initialize(int number)
+{
+	number = day;
+}
+
 int main()
 {
 	cout << "This program converts a day given by a number 1 through 365 into a month and a day." << endl << "Enter a number: ";
 
-	int day;
+	int num;
 
-	string month;
+	cin >> num;
 
-	cin >> day;
+	DayOfYear caller;
 
-	if(day > 0 && day < 32)
-	{
-		month = "January";
-		cout << month << " " << day << endl;
-	}
+	caller.initialize(num);
 
-	else if(day > 31 && day < 61)
-	{
-		month = "February";
-		day -= 31;
-		cout << month << " " << day << endl;
-	}
-
-	else if(day > 60 && day < 92)
-	{
-		month = "March";
-		day -= 60;
-		cout << month << " " << day << endl;
-	}
-
-	else if(day > 91 && day < 122)
-	{
-		month = "April";
-		day -= 91;
-		cout << month << " " << day << endl;
-	}
-
-	else if(day > 121 && day < 153)
-	{
-		month = "May";
-		day -= 121;
-		cout << month << " " << day << endl;
-	}
-
-	else if(day > 152 && day < 183)
-	{
-		month = "June";
-		day -= 152;
-		cout << month << " " << day << endl;
-	}
-
-	else if(day > 182 && day < 214)
-	{
-		month = "July";
-		day -= 182;
-		cout << month << " " << day << endl;
-	}
-
-	else if(day > 213 && day < 245)
-	{
-		month = "August";
-		day -= 213;
-		cout << month << " " << day << endl;
-	}
-
-	else if(day > 244 && day < 275)
-	{
-		month = "September";
-		day -= 244;
-		cout << month << " " << day << endl;
-	}
-
-	else if(day > 274 && day < 306)
-	{
-		month = "October";
-		day -= 274;
-		cout << month << " " << day << endl;
-	}
-
-	else if(day > 305 && day < 336)
-	{
-		month = "November";
-		day -= 305;
-		cout << month << " " << day << endl;
-	}
-
-	else if(day > 335 && day < 367)
-	{
-		month = "December";
-		day -= 335;
-		cout << month << " " << day << endl;	
-	}
-
-	else
-	{
-		die("date does not exist");
-	}
+	caller.print(num);
 }
 
 bool die(const string & msg)
