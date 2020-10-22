ac06af1d7f2efa75f3cb7044581e733d492ab251
Matthew <mgarcia495@yahoo.com>
2020-10-22 14:44:10 -0700

    added comment

--- Assignments/sum_string_digits.cpp
+++ Assignments/sum_string_digits.cpp
@@ -1,6 +1,14 @@
+/*
+   User inputs a series of single digit numbers
+   Using pointers, it is inputed as a C-String and dynamically allocated
+   adds all of the single digit numbers and returns it
+   also displays the highest and lowest digits in the string
+
+   CS216
+   Matthew Garcia
+*/
+
 #include <iostream>
-#include <sstream>
-#include <string>
 
 using namespace std;
 
@@ -9,7 +17,6 @@ int main()
 	cout << "Enter a number with less than 50 digits" << endl;
 
 	const int LENGTH = 50;
-
 	char *numbers;
 
 	numbers = new char[LENGTH];
@@ -17,16 +24,29 @@ int main()
 	cin.getline(numbers, LENGTH);
 
 	int sum = 0;
+	int high = 0;
+	int low = *numbers - 48;
 
 	while(*numbers != '\0')
 	{
 		cout << *numbers;
 
 		sum += *numbers - 48;
+		
+		if((*numbers - 48) >= high)
+		{
+			high = *numbers - 48;
+		}
+
+		else if((*numbers - 48) <= low)
+		{
+			low = *numbers - 48;
+		}
 
 		numbers++;
-
 	}
 
-	cout << endl << sum << endl;
+	cout << "\nThe sum of the numbers are " <<  sum << endl;
+	cout << "The highest digit is: " <<  high << endl;
+	cout << "The lowest digit is: " << low << endl;
 }
