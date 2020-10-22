876326318911a3c0b0ad77b9205173a23597d181
Matthew <mgarcia495@yahoo.com>
2020-10-22 14:07:58 -0700

    able to print out pointer string

---
+++ Assignments/sum_string_digits.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <sstream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+	cout << "Enter a number with less than 50 digits" << endl;
+
+	const int LENGTH = 50;
+
+	char *numbers;
+
+	numbers = new char[LENGTH];
+
+	cin.getline(numbers, LENGTH);
+
+	int sum = 0;
+
+	while(*numbers != '\0')
+	{
+		cout << *numbers;
+		numbers++;
+
+	}
+
+}
