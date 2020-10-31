783007f7fea73f4d12bab8dd03c07e35e5cac854
Matthew <mgarcia495@yahoo.com>
2020-10-30 21:24:20 -0700

    frequency

---
+++ Assignments/help.cpp
@@ -0,0 +1,55 @@
+#include<iostream>
+#include<fstream>
+#include<iomanip>
+
+using namespace std;
+
+int main()
+{
+	fstream File;
+	char Char;
+	char ASCII[128] = {0}; 
+	int frequency[128];
+	string input;
+
+	for(int ASCII = 0; ASCII < 128; ASCII++)
+	{
+		frequency[ASCII] = 0;
+	}
+
+	cout << "Enter file name: ";
+	cin >> input;
+
+	File.open(input);
+
+	if(!File) 
+	{
+		cout << "File not found!" << endl; 
+		exit(0);
+	}
+
+	while(File)
+	{
+		if(Char != ' ')
+		{
+			frequency[Char]++; 
+		}
+		Char = File.get();
+	}
+
+	File.close();
+
+	cout << "\nThe Frequency of the capital letters are: " << endl;
+
+	for(char upper = 65; upper <= 90; upper++)
+	{
+		cout << "Letter " << upper << " is " << setw(4) << frequency[upper] << endl;
+	}
+
+	cout << "\nThe Frequency of the lower case letters are: " << endl;
+
+	for(char lower = 97; lower <= 122; lower++)
+	{
+		cout << "Letter " << lower << " is " << setw(4) << frequency[lower] << endl;
+	}
+}
