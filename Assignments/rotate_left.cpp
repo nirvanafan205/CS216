c6d49ab7f85c052c34450602f754eeb7830fc859
Matthew <mgarcia495@yahoo.com>
2020-12-09 21:17:42 -0800

    all data types rotate

--- Assignments/rotate_left.cpp
+++ Assignments/rotate_left.cpp
@@ -3,15 +3,17 @@
 
 using namespace std;
 
-void sort(vector<int>& sorted);
+template<class T>
+void sort(vector <T>& sorted);
 
-void output(vector<int>& out);
+template<class T>
+void output(vector <T>& out);
 
 int main()
 {
 	vector <int> nums;
 
-	cout << "How many numbers do you want to input?" << endl;
+	cout << "How many numbers do you want to input?(ints)" << endl;
 
 	int amount;
 
@@ -33,9 +35,79 @@ int main()
 	{
 		sort(nums);
 	}
+
+	vector <double> nums2;
+
+	cout << "\nHow many numbers do you want to input?(doubles)" << endl;
+
+	int double_amount;
+
+	cin >> double_amount;
+
+	int double_limit = 0;
+	double double_input;
+
+	while(double_limit != double_amount)
+	{
+		cin >> double_input;
+		nums2.push_back(double_input);
+		double_limit++;
+	}
+
+	for(int b = 0; b < double_amount; b++)
+	{
+		sort(nums2);
+	}
+
+	vector <char> nums3;
+
+	cout << "\nHow many characters do you want to input?" << endl;
+
+	int char_amount;
+
+	cin >> char_amount;
+
+	int char_limit = 0;
+	char char_input;
+
+	while(char_amount != char_limit)
+	{
+		cin >> char_input;
+		nums3.push_back(char_input);
+		char_limit++;
+	}
+
+	for(int c = 0; c < char_amount; c++)
+	{
+		sort(nums3);
+	}
+
+	vector <string> nums4;
+
+	cout << "\nHow many strings do you want to input?" << endl;
+
+	int string_amount;
+
+	cin >> string_amount;
+
+	int string_limit = 0;
+	string string_input;
+
+	while(string_amount != string_limit)
+	{
+		cin >> string_input;
+		nums4.push_back(string_input);
+		string_limit++;
+	}
+
+	for(int s = 0; s < string_amount; s++)
+	{
+		sort(nums4);
+	}
 }
 
-void sort(vector<int>& sorted)
+	template<class T>
+void sort(vector <T>& sorted)
 {
 	output(sorted);	
 
@@ -45,21 +117,22 @@ void sort(vector<int>& sorted)
 	{
 		if(i == 0)
 		{
-			int temp = sorted[size - 1];
+			T temp = sorted[size - 1];
 			sorted[size - 1] = sorted[i];
 			sorted[i] = temp;
 		}
 
 		else if( i < size)
 		{
-			int temp = sorted[size - (i + 1)];
+			T temp = sorted[size - (i + 1)];
 			sorted[size - (i + 1)] = sorted[0];
 			sorted[0] = temp;
 		}
 	}
 }
 
-void output(vector<int>& out)
+	template<class T>
+void output(vector <T>& out)
 {
 	for(int b = 0; b < out.size(); b++)
 	{
